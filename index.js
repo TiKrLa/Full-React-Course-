c46e0a6520e6392de47731d2688f7efd9dab2e5e
Tia Lapinjoki <tia.lapinjoki14@gmail.com>
2021-08-11 16:36:48 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+// JSX Rules
+// return single element 
+// div / section / article for Fragment 
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
+
+function Greeting() {
+ return (
+  <div>
+    <h1>hello world</h1>
+   </div>
+ );
+ }
+
+ReactDom.render(<Greeting />, document.getElementById('root'));
