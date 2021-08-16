18471fc494ec4bf15b7364c4ea622587e0e5f2bb
Tia <56343426+TiKrLa@users.noreply.github.com>
2021-08-16 16:15:57 +0300

    Updated inxdex.js and index.css

--- index.js
+++ index.js
@@ -4,37 +4,51 @@ import ReactDom from 'react-dom';
 
 // CSS
 import './index.css';
+// setup vars
+const firstBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81GqtNbs%2BPL._AC_UL200_SR200,200_.jpg',
+  title: 'Billy Summerssss',
+  author: 'Stephen King, Paul Sparks',
+};
+const secondBook = {
+  img: 'https://m.media-amazon.com/images/I/81BC-VqUZfL._AC_UY327_FMwebp_QL65_.jpg',
+  title: 'The Housekeeper',
+  author: 'Natalie Barelli',
+};
+
 function BookList() {
  return (
   <section className='booklist'>
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
+    <Book 
+      img={firstBook.img} 
+      title={firstBook.title} 
+      author={firstBook.author} 
+    >
+      <p>
+        Lorem elit consectetur dolore qui elit duis dolore esse qui. 
+        Eu ipsum ut sit non pariatur aute fugiat officia consequat duis. 
+        Irure pariatur aliqua dolore id nisi fugiat eu proident amet ea. 
+        Laboris dolor magna labore ea enim nostrud est. Quis nulla Lorem eiusmod ullamco sint eu quis dolore. Lorem pariatur aute Lorem do officia adipisicing est.
+      </p>
+    </Book>
+    <Book 
+      img={secondBook.img} 
+      title={secondBook.title}
+      author={secondBook.author} 
+    />
   </section>
   );
- }
-
- const Book = () => {
+}
+const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props;
   return (
     <article className='book'>
-      <Image></Image>
-      <Title />
-      <Author />
+     <img src={img} alt=''/>
+     <h1>{title}</h1>
+     <h4>{author}</h4>
     </article>
     );
  };
- const Image = () => (
-  <img 
-    src='https://images-na.ssl-images-amazon.com/images/I/81GqtNbs%2BPL._AC_UL200_SR200,200_.jpg'
-    alt=''
-  />
- );
-
- const Title = () => <h1>Billy Summers</h1>
- const Author = () => <h4 style={{color: '#617d98', fontSize:'0.75rem', marginTop:'0.25rem' }}>Stephen King, Paul Sparks</h4>
+ 
 ReactDom.render(<BookList/>, document.getElementById('root')
 );
