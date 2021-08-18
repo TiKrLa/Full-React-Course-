0746582154ec322e7de8631c05fd80041f28efe8
Tia <56343426+TiKrLa@users.noreply.github.com>
2021-08-18 18:00:31 +0300

    Update index.js and index.css

--- index.js
+++ index.js
@@ -1,46 +1,41 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-
 // CSS
 import './index.css';
-// setup vars
-const firstBook = {
+const books = [
+{ id:1,
   img: 'https://images-na.ssl-images-amazon.com/images/I/81GqtNbs%2BPL._AC_UL200_SR200,200_.jpg',
-  title: 'Billy Summerssss',
+  title: 'Billy Summers',
   author: 'Stephen King, Paul Sparks',
-};
-const secondBook = {
+},
+{
+  id:2,
   img: 'https://m.media-amazon.com/images/I/81BC-VqUZfL._AC_UY327_FMwebp_QL65_.jpg',
   title: 'The Housekeeper',
   author: 'Natalie Barelli',
-};
+ },
+{
+  id:3,
+  img: 'https://m.media-amazon.com/images/I/61-fpk2vo8L._AC_UL480_FMwebp_QL65_.jpg',
+  title: 'The Perfect Wife',
+  author: 'Blake Pierce',
+ },
+];
 
 function BookList() {
  return (
-  <section className='booklist'>
-    <Book 
-      img={firstBook.img} 
-      title={firstBook.title} 
-      author={firstBook.author} 
-    >
-      <p>
-        Lorem elit consectetur dolore qui elit duis dolore esse qui. 
-        Eu ipsum ut sit non pariatur aute fugiat officia consequat duis. 
-        Irure pariatur aliqua dolore id nisi fugiat eu proident amet ea. 
-        Laboris dolor magna labore ea enim nostrud est. Quis nulla Lorem eiusmod ullamco sint eu quis dolore. Lorem pariatur aute Lorem do officia adipisicing est.
-      </p>
-    </Book>
-    <Book 
-      img={secondBook.img} 
-      title={secondBook.title}
-      author={secondBook.author} 
-    />
-  </section>
-  );
+ <section className='booklist'>
+   {books.map((book,index) => {
+     return <Book key={book.id} {...book}></Book>;
+ })}
+ </section>
+ );
 }
-const Book = ({ img, title, author }) => {
-  // const { img, title, author } = props;
+
+const Book = ({img,title,author}) => {
+  //const { img, title, author } = props;
+
   return (
     <article className='book'>
      <img src={img} alt=''/>
