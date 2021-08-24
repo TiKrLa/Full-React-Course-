77ff68dc489839f0810d3885cc976016b6a85e77
Tia <56343426+TiKrLa@users.noreply.github.com>
2021-08-24 15:59:37 +0300

    Created new file

---
+++ books.js
@@ -0,0 +1,20 @@
+export const data = [
+{ 
+  id:1,
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81GqtNbs%2BPL._AC_UL200_SR200,200_.jpg',
+  title: 'Billy Summers',
+  author: 'Stephen King, Paul Sparks',
+},
+{
+  id:2,
+  img: 'https://m.media-amazon.com/images/I/81BC-VqUZfL._AC_UY327_FMwebp_QL65_.jpg',
+  title: 'The Housekeeper',
+  author: 'Natalie Barelli',
+ },
+{
+  id:3,
+  img: 'https://m.media-amazon.com/images/I/61-fpk2vo8L._AC_UL480_FMwebp_QL65_.jpg',
+  title: 'The Perfect Wife',
+  author: 'Blake Pierce',
+ },
+];
--- index.js
+++ index.js
@@ -3,62 +3,20 @@ import ReactDom from 'react-dom';
 
 // CSS
 import './index.css';
-const books = [
-{ id:1,
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81GqtNbs%2BPL._AC_UL200_SR200,200_.jpg',
-  title: 'Billy Summers',
-  author: 'Stephen King, Paul Sparks',
-},
-{
-  id:2,
-  img: 'https://m.media-amazon.com/images/I/81BC-VqUZfL._AC_UY327_FMwebp_QL65_.jpg',
-  title: 'The Housekeeper',
-  author: 'Natalie Barelli',
- },
-{
-  id:3,
-  img: 'https://m.media-amazon.com/images/I/61-fpk2vo8L._AC_UL480_FMwebp_QL65_.jpg',
-  title: 'The Perfect Wife',
-  author: 'Blake Pierce',
- },
-];
+
+import {data} from './books'
+import SpecificBook from './Book'
+import {greeting} from './tests/tests'
 
 function BookList() {
+ console.log(greeting);
  return (
  <section className='booklist'>
-   {books.map((book,index) => {
-     return <Book key={book.id} {...book}></Book>;
+   {data.map((book,index) => {
+     return <SpecificBook key={book.id} {...book}></SpecificBook>;
  })}
  </section>
  );
 }
-
-const Book = ({img,title,author}) => {
-  // attribute, eventHandler 
-  // onClick, onMouseOver
-  const clickHandler = (e) => {
-    console.log(e);
-    console.log(e.target);
-    alert('hello world');
-  };
-  const complexExample = (author) => {
-    console.log(author);
-  };
-  return (
-    <article className='book' onMouseOver={()=>{
-    console.log();
-    }}>
-     <img src={img} alt=''/>
-     <h1 onClick={()=> console.log(title)}>{title}</h1>
-     <h4>{author}</h4>
-     <button type="button" onClick={clickHandler}>
-       reference example
-     </button>
-     <button type="button" onClick={() => complexExample(author)}>more 
-     complex example</button>
-    </article>
-    );
- };
- 
 ReactDom.render(<BookList/>, document.getElementById('root')
 );
